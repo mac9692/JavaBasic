7d1e9868ebf2fd6e3f05a7b93a4746ad79d72107
DESKTOP-FKHPESP\JinSeong <mac9692@nate.com>
2022-09-01 22:05:56 +0900

    2022.09.01 Method References

---
+++ src/methodreferences/ArrangeList2.java
@@ -0,0 +1,19 @@
+package methodreferences;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ArrangeList2 {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 3, 5, 7, 9);
+        integerList = new ArrayList<>(integerList);
+
+        Consumer<List<Integer>> consumer = Collections::reverse;
+        consumer.accept(integerList);
+
+        System.out.println(integerList);
+    }
+}
